9cafcec58ca109a95596a28ed83c4a846262f71c
Kasama <robertoaall@gmail.com>
2023-03-23 12:10:10 -0300

    feat(docs/hover): add support for some html escape sequences
    Implements #22757
    
    During handling of textDocument/hover, if the language server returns a
    payload containing markdown with some html escape sequences, the neovim
    handler will convert common ones to characters to display in the
    floating window's buffer.
    
    Signed-off-by: Kasama <robertoaall@gmail.com>

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -1378,6 +1378,21 @@ function M.stylize_markdown(bufnr, contents, opts)
     end
   end
 
+  -- Handle some common html escape sequences
+  stripped = vim.tbl_map(
+    function(line)
+      line = string.gsub(line, '&gt;', '>')
+      line = string.gsub(line, '&lt;', '<')
+      line = string.gsub(line, '&quot;', '"')
+      line = string.gsub(line, '&apos;', "'")
+      line = string.gsub(line, '&ensp;', ' ')
+      line = string.gsub(line, '&emsp;', ' ')
+      line = string.gsub(line, '&amp;', '&')
+      return line
+    end,
+    stripped
+  )
+
   -- Compute size of float needed to show (wrapped) lines
   opts.wrap_at = opts.wrap_at or (vim.wo['wrap'] and api.nvim_win_get_width(0))
   local width = M._make_floating_popup_size(stripped, opts)
